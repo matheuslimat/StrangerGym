20fd20c18428dae6111cf1bf19b6b2cbcba524eb
matheuslimat <matheuslimat@gmail.com>
2019-11-17 19:11:18 -0300

    Configurando SWAGGER

--- src/main/java/com/pp/strangergym/controllers/TreinoController.java
+++ src/main/java/com/pp/strangergym/controllers/TreinoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,30 +19,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pp.strangergym.models.Treino;
 import com.pp.strangergym.repository.TreinoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api") // tem q ser um substantivo n pode ser verbo
+@Api(value = "API REST Academia") // anotação do swagger
+@CrossOrigin(origins = "*") // qualquer protocolo acessa essa api n só http
 public class TreinoController {
 	
 	@Autowired
 	public TreinoRepository tr;
 	
 	@GetMapping("/listarTreinos")
+	@ApiOperation(value = "Retorna uma lista de Treinos de todo mundo")
 	public @ResponseBody Iterable<Treino> listarTreinos() { // 
 		Iterable<Treino> listaTreinos = tr.findAll();
 		return listaTreinos;
 	}
 	
 	@GetMapping("/listarTreinos/{idTreino}")
+	@ApiOperation(value = "Retorna uma treino quando especificado um idTreino")
 	public @ResponseBody Optional<Treino> getTreino(@PathVariable("idTreino") Long idTreino) { // 
 		return tr.findById(idTreino);
 	}
 	
 	@PostMapping("/cadastrarTreino")
+	@ApiOperation(value = "Cadastra um treino")
 	public Treino cadastrarTreino(@RequestBody @Valid Treino treino) {
 		return tr.save(treino);
 	}
 	
 	@DeleteMapping("/treino")
+	@ApiOperation(value = "Deleta um treino passando o Json dele")
 	public Treino deletaTreino(@RequestBody Treino treino) {
 		tr.delete(treino);
 		// retorna como resposta o evento q foi deletado
@@ -49,6 +59,7 @@ public class TreinoController {
 	}
 	
 	@PutMapping("/treino")
+	@ApiOperation(value = "Atualiza o treino passando o Json dele")
 	public Treino atualizarTreino(@RequestBody Treino treino) {
 		return tr.save(treino);
 	}
